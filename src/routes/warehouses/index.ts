bf218fd6c28374478f386d8111df926231dd27ac
Bjorn Francke <bjorn@welcomebob.com>
2024-03-27 11:00:31 +0100

    Adds warehouse routes: delete, create and findById

--- src/routes/warehouses/index.ts
+++ src/routes/warehouses/index.ts
@@ -13,5 +13,42 @@ warehouseRouter.route('/findAll')
     }
 })
 
+warehouseRouter.route('/delete/:warehouseId')
+    .delete(async (req, res) => {
+        try {
+            const deletedWarehouse = await Warehouse.findByIdAndDelete(req.params.warehouseId);
+            res.json(deletedWarehouse)
+        } catch {
+            res.status(500).send("Internal Server Error");
+        }
+    })
+
+warehouseRouter.route('/create')
+    .post(async (req, res) => {
+        try {
+            const newWarehouse = new Warehouse(
+                {
+                    name: req.body.name,
+                    prefix: req.body.prefix,
+                    items: []
+                }
+            );
+            const savedWarehouse = await newWarehouse.save();
+            res.json(savedWarehouse);
+        } catch {
+            res.status(500).send("Internal Server Error");
+        }
+    })
+
+warehouseRouter.route('/findById/:warehouseId')
+    .get(async (req, res) => {
+        try {
+            const warehouseData = await Warehouse.findById(req.params.warehouseId)
+            res.json(warehouseData)
+        } catch {
+            res.status(500).send("Internal Server Error")
+        }
+    })
+
 
 export default warehouseRouter
