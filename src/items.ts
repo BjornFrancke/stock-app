3cc48f78ad99ce42c3589aa95825ddfea717a0b4
Bjorn Francke <bjorn@welcomebob.com>
2024-03-27 10:46:10 +0100

    Adds export to Item model and add function to insert sample Data

--- src/items.ts
+++ src/items.ts
@@ -2,20 +2,34 @@ import {Schema, Model, model, HydratedDocument} from "mongoose"
 import {Iitems} from "./types";
 
 type ItemsModel = Model<Iitems>
-const itemSchema = new Schema<Iitems, ItemsModel>({
+export const itemSchema = new Schema<Iitems, ItemsModel>({
     name: {type: String},
     description: {type: String},
     stock: {type: Number}
 })
 
 
-const Item: ItemsModel = model<Iitems, ItemsModel>('Item', itemSchema)
+export const Item: ItemsModel = model<Iitems, ItemsModel>('Item', itemSchema)
 
 export const item: HydratedDocument<Iitems> = new Item({
     name: "PSU",
     description: "1000 watt power supply",
     stock: 3
 })
+
+export function createItem() {
+    const testData: HydratedDocument<Iitems> = new Item({
+        name: "PSU",
+        description: "1000 watt power supply",
+        stock: 3
+    })
+   testData.save()
+}
+
+export function listAllItems() {
+    const responds = Item.find()
+    console.log(responds)
+}
 
 export const logItemData = (item: HydratedDocument<Iitems>): void => {
     console.log("Name: " + item.name)
