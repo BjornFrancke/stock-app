5d117729f534e2287524d4160d472f42ffd641dc
Bjorn Francke <bjorn@welcomebob.com>
2024-03-27 12:31:48 +0100

    Adds delete button to ListAllItems and temp types in frontend

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,25 +1,45 @@
 import './index.css'
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import {Iitems} from "./types.ts";
 
-const ListAllItems = () => {
-    const [items, setItems] = useState([])
+const NewListAllItems = () => {
+    const [items, setItems] = useState([]);
 
+    // Fetch items initially
     useEffect(() => {
-        const apiCall = () => {
-            axios.get('http://localhost:3000/item/findAll').then((data) => {
-                // @ts-ignore
-                setItems(data.data.map((result) => <li key={result.id}>{result.name}</li>))
-            })
-        }
+        fetchItems();
+    }, []);
 
-        apiCall()
-    }, [])
+    // Make the apiCall a separate function so it can be called again after delete
+    const fetchItems = async () => {
+        const response = await axios.get('http://localhost:3000/item/findAll');
+        setItems(response.data);
+    };
+
+    // Item deletion handler
+    const handleDelete = async (id: string | undefined) => {
+        // Send a DELETE request
+        if (typeof id === 'undefined') {
+            console.warn('Cannot delete an item without an id');
+            return;
+        }
+        await axios.delete(`http://localhost:3000/item/delete/${id}`);
+        // After deleting the item, fetch items again to refresh the list
+        fetchItems();
+    };
 
     return (
-        <ul>{items}</ul>
-    )
-}
+        <ul>
+            {items.map((item: Iitems) => (
+                <li key={item._id}>
+                    {item.name} - {item.stock}{' '}
+                    {item._id && <button onClick={() => handleDelete(item._id)}>Delete</button>}                </li>
+            ))}
+        </ul>
+    );
+};
+
 
 
     function App() {
@@ -28,7 +48,7 @@ const ListAllItems = () => {
       <>
           <h1>hello</h1>
           <div className={"bg-gray-300 w-fit h-fit p-6"}>
-              <ListAllItems/>
+              <NewListAllItems/>
           </div>
       </>
   )
---
+++ apps/frontend/src/types.ts
@@ -0,0 +1,7 @@
+export interface Iitems {
+    _id? : string | undefined,
+    name: string,
+    description?: string,
+    stock: number,
+}
+
