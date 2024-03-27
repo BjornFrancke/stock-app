11c4e464c5a81f89d5aa33c0b0b3facb7f50bd3d
Bjorn Francke <bjorn@welcomebob.com>
2024-03-27 12:14:56 +0100

    Add ListAllItems component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,21 +1,35 @@
-import axios from 'axios'
 import './index.css'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
+const ListAllItems = () => {
+    const [items, setItems] = useState([])
 
-const apiCall = () => {
-    axios.get('http://localhost:3000/item/findAll').then((data) => {
-        //this console.log will be in our frontend console
-        console.log(data)
-    })
+    useEffect(() => {
+        const apiCall = () => {
+            axios.get('http://localhost:3000/item/findAll').then((data) => {
+                // @ts-ignore
+                setItems(data.data.map((result) => <li key={result.id}>{result.name}</li>))
+            })
+        }
+
+        apiCall()
+    }, [])
+
+    return (
+        <ul>{items}</ul>
+    )
 }
 
-function App() {
 
-  return (
+    function App() {
+
+        return (
       <>
           <h1>hello</h1>
-          <button onClick={apiCall}>Make API Call</button>
-
+          <div className={"bg-gray-300 w-fit h-fit p-6"}>
+              <ListAllItems/>
+          </div>
       </>
   )
 }
