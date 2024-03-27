33ec5095d07175a3e745fa1465b16c198acfd4bd
Bjorn Francke <bjorn@welcomebob.com>
2024-03-27 10:11:49 +0100

    Moves express port variable to config file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from "express";
 import itemRouter from "./routes/items";
-import {item, logItemData} from "./items";
 import mongoose from "mongoose";
-import {DATABASE_URL} from "./creds";
+import {appPort, DATABASE_URL} from "./creds";
 
 mongoose.connect(DATABASE_URL)
 const database = mongoose.connection
@@ -15,12 +14,11 @@ database.once('connected', () => {
     console.log('Database Connected');
 })
 
+
 const app = express()
-const port = 3000
+const port = appPort
 app.use(express.json())
 
-
-logItemData(item)
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
