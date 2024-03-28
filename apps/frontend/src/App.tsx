3a5d4cd3d43cd21307072beb8a4a6561fd1ca058
Bjorn Francke <bjorn@welcomebob.com>
2024-03-28 11:23:52 +0100

    Adds react router

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,17 +1,21 @@
 import './index.css'
 import { ListAllItems } from "./components/ListAllItems";
 import { ListAllBoms } from './components/ListAllBoms';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout';
 
 
 function App() {
     return (
-        <>
-            <h1>Hello</h1>
-            <div className={"w-fit h-fit p-6"}>
-                <ListAllItems />
-                <ListAllBoms />
-            </div>
-        </>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+            <Route path="items" element={<ListAllItems/>}/>
+            <Route path="boms" element={<ListAllBoms/>}/>
+            </Route>
+            
+        </Routes>
+        </BrowserRouter>
     )
 }
 
