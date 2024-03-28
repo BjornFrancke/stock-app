074af5f6256291683809ec87e5171cba5036c059
Bjorn Francke <bjorn@welcomebob.com>
2024-03-28 16:51:27 +0100

    Adds bom routes, addComponent, delete bom

--- apps/backend/src/routes/bom/index.ts
+++ apps/backend/src/routes/bom/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Bom } from "../../bom";
+import { Item } from "../../items";
 
 const bomRouter = express.Router()
 
@@ -11,6 +12,62 @@ bomRouter.route('/findAll')
     } catch {
         res.status(500).send("Internal Server Error");
     }
+    })
+
+bomRouter.route('/findById/:bomId')
+.get(async(req, res) => {
+    const id = req.params.bomId
+    try{
+        const data = await Bom.findById(id)
+        res.json(data)
+    } catch {
+        res.status(500).send("internal Server Error")
+    }
+})
+
+
+bomRouter.route('/delete/:bomId')
+.delete(async (req, res) => {
+    const id = req.params.bomId
+    try {
+        const deletedBom = Bom.findByIdAndDelete(id)
+        res.json(deletedBom)
+    } catch {
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+bomRouter.route('/addComponent/:bomId')
+.patch(async (req, res) => {
+    const id = req.params.bomId
+    const newComponentId = req.body.componentId
+    const newComponentAmount: number = Number(req.body.componentAmount)
+    try {
+        const bom = await Bom.findById(id)
+        const newComponent = await Item.findById(newComponentId)
+        if (newComponent && bom){
+            bom?.components.push({id: newComponentId, amount: newComponentAmount})
+
+        } else {
+            throw new Error("Component not found");
+        }
+        const updateBom = await bom.save
+        res.send(updateBom)
+    } catch {
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+bomRouter.route('/setComponentAmount/:bomId/:componentId')
+    .patch(async (req, res) => {
+        const bomId = req.params.bomId
+        const componentId = req.params.componentId
+        try {
+            const bom = await Bom.findById(bomId)
+
+        } catch {
+            res.status(500).send("Internal Server Error")
+        }
     })
 
 
