c181327b7aaf71eb95d5bbe0376d3403584096a1
Bjorn Francke <bjorn@welcomebob.com>
2024-04-10 20:16:58 +0200

    Redesigns Home page

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,13 +1,31 @@
+import Sheet from "@mui/joy/Sheet";
 
 export function Home() {
     return (
         <>
-        <div className=" w-[80%] mx-auto flex justify-center pt-20">
-            <div>
-            <h1>Stock-App</h1>
+            <Sheet
+                className={"mx-auto mt-6"}
+                sx={{
+                    maxWidth: 800,
+                    borderRadius: "md",
+                    p: 3,
+                    boxShadow: "lg",
+                }}
+            >
+                <div>
+                    <h1>Welcome user!</h1>
+                </div>
+                <div className={"grid grid-cols-2 space-x-4 mt-4"}>
+                <div className={"border min-w-24 min-h-16"}>
+                    Pending Orders
+                </div>
+                <div className={"border min-w-24 min-h-16"}>
+                    Another stat
+                </div>
+                </div>
 
-            </div>
-        </div>
+            </Sheet>
+
         </>
     )
 }
