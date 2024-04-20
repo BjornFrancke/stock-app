bd8d07bc43f4cda5b2104a4c8000454460a116cd
Bjorn Francke <bjorn@welcomebob.com>
2024-04-20 18:07:26 +0200

    Refactor backend code to use controller methods in routes
    The backend code has been refactored to use controller methods instead of inline functions in route definitions. This has significantly reduced the lines of code in route files and improved their readability. This refactoring includes changes in manufacturing, customer, order, and items routes as well as corresponding controller files.

---
+++ apps/backend/src/controllers/customerController.ts
@@ -0,0 +1,55 @@
+import asyncHandler from "express-async-handler"
+import {Customer} from "../models/customers";
+
+export const getCustomers = asyncHandler(async (req, res) => {
+    try {
+        const data = await Customer.find()
+        res.json(data)
+    } catch (error) {
+        res.status(500).send("Internal Server Error " + error)
+    }
+})
+
+export const getCustomerById = asyncHandler(async (req, res) => {
+    const id = req.params.customerId
+    try {
+        const data = await Customer.findById(id)
+        res.json(data)
+    } catch (error) {
+        res.status(500).send("Internal Server Error " + error)
+    }
+})
+
+export const deleteCustomer = asyncHandler(async (req, res) => {
+    const id = req.params.customerId
+    try {
+        const customerData = await Customer.findById(id)
+        if (!customerData) {
+            res.status(404).send("Customer not found")
+        }
+        const deletedCustomer = await Customer.findByIdAndDelete(id)
+        res.json(deletedCustomer)
+    } catch (error) {
+        res.status(500).send("Internal Server Error " + error)
+    }
+})
+
+export const createCustomer = asyncHandler(async (req, res) => {
+    try {
+        const newCustomer = new Customer( {
+            name: req.body.name,
+            mailAdress: req.body.mailAdress,
+            phoneNr: req.body.phoneNr,
+            address: {
+                street: req.body.address.street,
+                zip: req.body.address.zip,
+                city: req.body.address.city,
+                country: req.body.address.country
+            }
+        })
+        const savedCustomer = await newCustomer.save()
+        res.json(savedCustomer)
+    } catch (error) {
+        res.status(500).send("Internal Server Error" + error)
+    }
+})
--- apps/backend/src/routes/orders/index.ts
+++ apps/backend/src/routes/orders/index.ts
@@ -3,99 +3,25 @@ import { Order } from "../../models/orders";
 import mongoose from "mongoose";
 import { Item } from "../../items";
 import {orderMarkedAsDone} from "../../orders";
+import {addItemToOrder, createOrder, deleteOrder, getOrders, markOrderAsDone} from "../../controllers";
 
 const ordersRoute = express.Router()
 
 
 ordersRoute.route('/findAll')
-.get(async (reg, res) => {
-    try {
-        const data = await Order.find()
-        res.json(data)
-    } catch {
-        res.status(500).send("Internal Server Error");
-    }
-})
+.get(getOrders)
 
 ordersRoute.route('/create')
-.post(async (req, res) => {
-    const creationDate = Date.now()
-    try {
-        const newOrder = new Order(
-            {
-                orderNumber: req.body.orderNumber,
-                items: [],
-                createtionDate: creationDate,
-                dueDate: req.body.dueDate,
-                receptian: req.body.receptian,
-                isDone: false
-            }
-        );
-        const savedOrder = await newOrder.save();
-        res.json(savedOrder);
-    } catch {
-        res.status(500).send("Internal Server Error");
-    }
-})
+.post(createOrder)
 
 ordersRoute.route('/markAsDone/:orderId')
-.patch(async (req, res) => {
-    const orderId = req.params.orderId
-    try {
-        console.log("Order found")
-        if (orderId) {
-            console.log("Order exist")
-            const updatedOrder = await orderMarkedAsDone(orderId)
-            res.status(400).send(updatedOrder)
-        } else {
-            throw new Error("Item not found");
-            res.status(404).send(Error)
-        }
-    } catch {
-        res.status(500).send("Internal Server Error");
-    }
-})
+.patch(markOrderAsDone)
 
 ordersRoute.route('/:orderId/additem')
-.patch(async (req, res) => {
-    const orderId = req.params.orderId;
-    const itemId = req.body.itemId
-    const amount = Number(req.body.amount);
-
-    if (!amount) {
-        return res.status(400).send("Item amount is required");
-    }
-
-    try {
-        const order = await Order.findById(orderId);
-
-        if (!order) {
-            return res.status(404).send("Order not found");
-        }
-
-        const newItem = { _id: itemId, amount: amount };
-        order.items.push(newItem);
-
-        const updatedOrder = await order.save(); 
-        res.json(updatedOrder); 
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
-});
+.patch(addItemToOrder);
 
 
 ordersRoute.route('/delete/:orderId')
-    .delete(async (req, res) => {
-        try {
-            const deletedOrder = await Order.findByIdAndDelete(req.params.orderId);
-            if (!deletedOrder) {
-                res.status(404).send("The order was not found")
-            }
-            res.json(deletedOrder)
-        } catch {
-            res.status(500).send("Internal Server Error");
-        }
-    })
+    .delete(deleteOrder)
 
 export default ordersRoute
