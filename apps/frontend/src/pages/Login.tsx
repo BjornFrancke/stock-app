98e76fe0ca5a3800bff3f83c0522ecc2f9319016
Bjorn Francke <bjorn@welcomebob.com>
2024-04-21 15:12:25 +0200

    Refactors login page

--- apps/frontend/src/pages/Login.tsx
+++ apps/frontend/src/pages/Login.tsx
@@ -6,14 +6,23 @@ import axios from "axios";
 import {useNavigate} from "react-router-dom";
 
 export function Login () {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [loginCredentials, setLoginCredentials] = useState({
+        email: "",
+        password: ""
+    })
     const navigate = useNavigate();
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLoginCredentials({
+            ...loginCredentials,
+            [e.target.name]: e.target.value,
+        })
+    }
 
     const handleLogin = async () => {
         const loginInfo = {
-            email: email,
-            password: password
+            email: loginCredentials.email,
+            password: loginCredentials.password,
         }
         console.log(loginInfo);
         const loggedIn = await axios.post("http://localhost:3000/user/login", loginInfo);
@@ -25,20 +34,31 @@ export function Login () {
 
     return (
         <>
-        <h1>User</h1>
-            <Sheet>
+            <Sheet
+                className={"mx-auto mt-6 space-y-4"}
+                sx={{
+                    maxWidth: 800,
+                    borderRadius: "md",
+                    p: 3,
+                    boxShadow: "lg",
+                }}
+            >
+                <h1 className="text-xl mb-12">Login</h1>
+
                 <form className={"max-w-[30%] mx-auto"}>
                     <Input
-                    type="text"
-                    placeholder="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                        type="text"
+                        name={"email"}
+                        placeholder="email"
+                        value={loginCredentials.email}
+                        onChange={(e) => handleChange(e)}
                     />
                     <Input
                         type="password"
+                        name={"password"}
                         placeholder="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        value={loginCredentials.password}
+                        onChange={(e) => handleChange(e)}
                     />
                     <Button onClick={() => handleLogin()}>Submit</Button>
                 </form>
