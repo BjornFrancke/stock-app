f21311ebdb5c5fc55fcef05cdb5910594fae4a51
Bjorn Francke <bjorn@welcomebob.com>
2024-08-18 12:54:02 +0200

    Clean up - itemsController.ts

--- apps/backend/src/controllers/itemsController.ts
+++ apps/backend/src/controllers/itemsController.ts
@@ -8,16 +8,15 @@ export const getItems = asyncHandler(async (_req, res) => {
         const items = await fetchAllItems();
         res.status(200).json(items);
     } catch (error) {
-        res.status(500).json({ message: "Failed to get items" });
+        res.status(500).json({message: "Failed to get items"});
     }
 })
-
 
 export const getItemsById = asyncHandler(async (req, res) => {
     const itemId = req.params.itemId;
     const item = await Item.findById(itemId);
     if (!item) {
-        res.status(404).send("Item not found");
+        res.status(404).json({message: "Item not found"});
         return;
     }
     res.status(200).json(item);
@@ -25,13 +24,13 @@ export const getItemsById = asyncHandler(async (req, res) => {
 
 export const deleteItem = asyncHandler(async (req, res) => {
     const itemId = req.params.itemId;
-    const isItemUsedInBom = await Bom.findOne({ "components.id": itemId });
+    const isItemUsedInBom = await Bom.findOne({"components.id": itemId});
     if (isItemUsedInBom) {
-        res.status(403).send("Item is used in a BOM and cannot be deleted");
+        res.status(403).json({message: "Item is used in a BOM and cannot be deleted"});
         return
     }
     const deletedItem = await Item.findByIdAndDelete(itemId);
-    res.status(200).send(`${deletedItem?.name} was deleted`);
+    res.status(202).send(`${deletedItem?.name} was deleted`);
 })
 
 export const setItemStock = asyncHandler(async (req, res) => {
@@ -41,7 +40,7 @@ export const setItemStock = asyncHandler(async (req, res) => {
         const updatedItem = await setStock(itemId, newStock);
         res.status(200).json({message: `${updatedItem?.name}'s stock was updated`});
     } catch (error) {
-        res.status(500).json({ message: "Failed to update item stock" });
+        res.status(500).json({message: "Failed to update item stock"});
     }
 })
 
@@ -59,25 +58,20 @@ export const setItemPrice = asyncHandler(async (req, res) => {
         }
         item.salePrice = newPrice;
         await item.save();
-        res.status(200).json({message: "Item was updated"})
+        res.status(201).json({message: "Item was updated"})
     } catch (error) {
-        res.status(500).json({ message: "Failed to update item price" });
+        res.status(500).json({message: "Failed to update item price"});
     }
 })
 
 export const createItem = asyncHandler(async (req, res) => {
     try {
-        const newItem = new Item(
-            {
-                name: req.body.name,
-                description: req.body.description,
-                stock: req.body.stock
-            }
-        );
+        const {name, description, stock} = req.body;
+        const newItem = new Item({name, description, stock});
         const savedItem = await newItem.save();
         res.status(201).json({message: `${savedItem?.name} was created`, ...savedItem});
     } catch {
-        res.status(500).send("Internal Server Error");
+        res.status(500).json({message: "Internal Server Error"});
     }
 })
 
@@ -96,7 +90,6 @@ export const updateItemDescription = asyncHandler(async (req, res) => {
 
 })
 
-
 export const updateItemById = asyncHandler(async (req, res) => {
     try {
         const id = req.params.itemId
@@ -106,7 +99,7 @@ export const updateItemById = asyncHandler(async (req, res) => {
         const results = await Item.findByIdAndUpdate(
             id, updatedData, options
         )
-        res.send(results)
+        res.status(201).send(results)
     } catch (error) {
         res.status(400).json({message: error})
     }
